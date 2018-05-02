9b48c40a6fd78fff7bcf8dc534943c38f275f386
kbelisle <belisle57@msn.com>
2018-05-01 20:43:22 -0400

    Ajout DAL + modif JSON
    Ajout fichier DAL.php
    - fonction getAllCategories()
    - fonction getObjetById(id)
    
    Modif JSON
    -Content-type a application/JSON
    -Structure d'analyse de l'URI

---
+++ ProjetFinal_API/DAL.php
@@ -0,0 +1,60 @@
+<?php
+
+function getAllCategories($conn) {
+    $result = pg_query($conn, "SELECT * FROM projet.categorie");
+    if (!$result) {
+        pg_close($conn);
+        die (responseJson("5", "Error on request : Categorie/All", "[]"));
+    }
+    $data = pg_fetch_all($result);
+    if(!$data) {
+        pg_close($conn);
+        die (responseJson("5", "Error on request : Categorie/All", "[]"));
+    }
+    return json_encode($data,JSON_UNESCAPED_UNICODE);
+}
+
+function getObjetById($conn, $id) {
+    $result = pg_query_params($conn, "SELECT * FROM projet.objet WHERE idobj = $1", array($id));
+    if (!$result) {
+        pg_close($conn);
+        die (responseJson("5", "Error on request : objet/id", "[]"));
+    }
+    $data = pg_fetch_assoc($result);
+    if(!$data) {
+        $error = pg_last_error($conn);
+        pg_close($conn);
+        if ($error == "") {
+            /* No Result*/
+            die (responseJson("1", "Error on request : objet/id. No Result." . $error, "[]"));
+        }
+        else {
+            /*Error*/
+            die (responseJson("5", "Error on request : objet/id. " . $error, "[]"));
+        }
+    }
+    $result = pg_query_params($conn, "SELECT * FROM projet.feature WHERE idobj = $1", array($data["idobj"]));
+    if (!$result) {
+        pg_close($conn);
+        die (responseJson("5", "Error on request : Features on objet/id", "[]"));
+    }
+    $features = pg_fetch_all($result);
+    if(!$features) {
+        $error = pg_last_error($conn);
+        pg_close($conn);
+        if ($error == "") {
+            /*No Result*/
+            $data["features"] = [];
+        }
+        else {
+            /*Error*/
+            die (responseJson("5", "Error on request : objet/id. " . $error, "[]"));
+        }
+    }
+    else {
+        $data["features"] = $features;
+    }
+    return json_encode($data,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_LINE_TERMINATORS);
+}
+
+?>
--- ProjetFinal_API/index.php
+++ ProjetFinal_API/index.php
@@ -1,13 +1,15 @@
 <?php include "utility.php" ?>
+<?php include "DAL.php" ?>
 <?php
-
+/* Deny access to all pages except this one in .htaccess */
 // get the HTTP method, path and body of the request
+header('Content-Type: application/json');
 $method = $_SERVER['REQUEST_METHOD'];
 
 if(isset($_SERVER['PATH_INFO']))
     $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 else
-    $request = ["/"];
+    die(responseJson("3", "Empty Request", "[]"));
 
 //Get Json Input
 $input = json_decode(file_get_contents('php://input'),true);
@@ -19,18 +21,54 @@ $input = json_decode(file_get_contents('php://input'),true);
 
 /*Validation des requêtes ici*/
 
+/* Fill Data*/
 //Hide this in web.config on app deploy
 //Modifié connection_string pour pointé vers votre BD.
 $connection_string = 'host=postgres.iro.umontreal.ca port=5432 dbname=belislek user=belislek_app password=Projet_final_groupe_14';
 $DB = pg_connect($connection_string) or die ('Could not connect to BD : ' . pg_last_error());
-        
-pg_set_client_encoding($DB,"UTF-8");
 
-/* Fill Data  ici*/
-
-$data = "[]";
-
-pg_close($DB);
+$request_count = count($request);
+if ($request_count == 0) {
+    /* Invalid (Empty Request) */
+    pg_close($DB);
+    die(responseJson("3", "Empty Request", "[]"));
+}
+elseif ($request_count > 0 && $request[0] == "categorie") {
+    /* Request categorie*/
+    if($request_count == 1 && $method == "GET") {
+        /* All */
+        $data = getAllCategories($DB);
+        pg_close($DB);
+    }
+    else {
+        /* Invalide */
+        pg_close($DB);
+        die(responseJson("4", "Invalid Argument for categorie", "[]"));
+    }
+}
+else if($request_count > 0 && $request[0] == "objet") {
+    if ($request_count == 2 && $method == "GET") {
+        /* Get by Id */
+        if (ctype_digit($request[1])) {
+            $data = getObjetById($DB, intval($request[1]));
+            pg_close($DB);
+        }
+        else {
+            /* Invalide */
+            pg_close($DB);
+            die(responseJson("4", "Invalid Argument for objet/id", "[]"));
+        }
+    }
+    else {
+        /* Invalide */
+        pg_close($DB);
+        die(responseJson("4", "Invalid Argument for objet", "[]"));
+    }
+}
+else {
+    pg_close($DB);
+    die(responseJson("6", "Invalid Request. Check the request URI", "[]"));
+}
 
 echo responseJson(0, '', $data);
 
